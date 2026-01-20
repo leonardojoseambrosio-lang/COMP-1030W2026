f4cd87b9a985693b81aeb16779a5fe998e4d4392
leonardojoseambrosio-lang <leonardojoseambrosio@gmail.com>
2026-01-19 21:53:58 -0500

    Lab3 - Leonardo Jose Ambrosio - 200657215 Date: Jan/19/2026 Comment: Variables to hold the surface area declarated

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -16,16 +16,27 @@ public class Program
 
         //Variables - Planets Radius
         //Site with the values - https://www.wolframalpha.com/input?i=planets+radius
-        double mercuryRadius = 2440; //Km
-        double venusRadius = 6050; //Km
+        double mercuryRadius = 2440.0; //Km
+        double venusRadius = 6050.0; //Km
         double earthRadius = 6371.009; //Km
         double marsRadius = 3390; //Km
-        double jupterRadius = 69950; //Km
-        double saturnRadius = 58300; //Km
-        double uranusRadius = 25360; //Km
-        double neptuneRadius = 24600; //Km
+        double jupterRadius = 69950.0; //Km
+        double saturnRadius = 58300.0; //Km
+        double uranusRadius = 25360.0; //Km
+        double neptuneRadius = 24600.0; //Km
 
         // STEP 2: For each of the above variables, declare a new variable to hold the surface area using an appropriate real number type
+        //Variables to hold the surface area
+        double mercurySurfArea;
+        double venusSurfArea;
+        double earthSurfArea;
+        double marsSurfArea;
+        double jupterSurfArea;
+        double saturnSurfArea;
+        double uranusSurfArea;
+        double neptuneSurfArea;
+
+
         /* STEP 3: Set each of the above variables declared in STEP 2 to the surface area using an expression
         * (Hint: The formula for the surface area of a sphere is A=4πr2 (use Math.PI instead of π)
         */
