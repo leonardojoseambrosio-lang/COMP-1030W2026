9293399667c9f70541ddf9783d3950fee8b0ab35
leonardojoseambrosio-lang <leonardojoseambrosio@gmail.com>
2026-01-26 12:19:06 -0500

    Module 4

---
+++ Module4/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Module4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // STEP 1: Create a random number between 0 and 24 to represent the hour of the day
+
+            // STEP 2: Introducing the boolean type - declare a boolean variable to see if it is time to wake up
+
+            // STEP 3a: Build an if/else structure (a conditional statement) that outputs a message indicating whether it is time to wake up or not
+
+            // STEP 3b: Note that spacing with above lines is optional - it is formatting to make code easier to read
+
+            /* STEP 4: Relational operators (<, >, <=, >=) are used to compare two values: Construct an if/else structure to determine if it is the morning or the afternoon*/
+
+            // STEP 5: Equality operators (==, !=) can also be used in a conditional statement
+
+            // STEP 6: There is sometimes an issue when comparing real numbers (add 0.1 to 0.2 and compare it to 0.3 - this will evaluation to false as the expression evaluates to 0.30000000000000004)
+
+            // STEP 7a: Comparing Strings - get the day of the week
+
+            // STEP 7b: Output the day of the week
+
+            // STEP 7c: Create a condition to check to if today is Monday
+
+            /* STEP 8a: Comparing strings is a case-sensitive matter
+            ﻿* Declare a string variable to capture your name*/
+
+            // STEP 8b: Capture the name of the user from the console with Console.ReadLine()
+
+            // STEP 8c: Output what the user typed in
+
+            // STEP 8d: Write a condition to compare a FULL CAPS version of your name to what was typed in
+
+            // STEP 9: A code block of one or more statements is delimited using if (), and then{ and }
+
+            // STEP 10a: Variables have scope - that is they may be only accessible within the code block within which they were created - create a local string variable
+
+            // STEP 10b: Attempt to access the above local variable
+
+            // STEP 11: There are more complicated logical operators (&, &&, |, ||, ^) - let's try the and (&) operator:﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿﻿
+        }
+    }
+}
