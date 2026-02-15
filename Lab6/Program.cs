3acc1eec7828c6d02d504eaadc9be31ce5695d8a
leonardojoseambrosio-lang <leonardojoseambrosio@gmail.com>
2026-02-14 19:57:53 -0500

    Lab6 - Leonardo Jose Ambrosio - 200657215 Date: Feb/14/2026 Comment: Testing STEP 3.

--- Lab6/Program.cs
+++ Lab6/Program.cs
@@ -5,8 +5,19 @@ public class Program{
     public static void Main(string[] args) {
 
         // STEP 1: Declare a new array (of type string), and initialize it with anywhere from 7 to 9 different ice cream flavors;
+        string[] flavors = new string[7];
+        flavors = new string[] {"Chocolate", "Strawberry", "Raspberry", "Blackberry", "Banana", "Mango", "Lime"};
+
         // STEP 2: Create a variable to contain the Length property of the array you created in STEP 1
+        int flavorsLength = flavors.Length;
+
         // STEP 3: Output a simple sentence to the Console indicating to the user that you are about to list all the ice cream flavors available at your stand using a FOR() loop
+        Console.WriteLine($"See bellow the ice cream flavors available:");
+        for (int i = 0; i < flavorsLength; i++) 
+        {
+        Console.WriteLine( flavors[i] );
+        }
+
         // STEP 4: Build a FOR() loop to iterate through the array you created in STEP 1, using the variable you created in STEP 2 to control how many times the loop repeats
         // STEP 5: Inside the FOR() loop, output each element of the array to the Console
         // STEP 6: Output a simple sentence to the Console indicating to the user that you are about to list all the ice cream flavors available at your stand using a FOREACH() loop 
@@ -19,5 +30,4 @@ public class Program{
 
 
 
-
-}
+    }
--- Lab6/obj/Debug/net10.0/Lab6.AssemblyInfo.cs
+++ Lab6/obj/Debug/net10.0/Lab6.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5e4900fbefc97d4ee5daaccac6467749a819389")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d127d696546844eb6c7b720a2c9f78a6eab4c1e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
