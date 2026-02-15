c8c2c3c4a6eb9980365840b26f1eb31b70bad645
leonardojoseambrosio-lang <leonardojoseambrosio@gmail.com>
2026-02-14 19:37:00 -0500

    Lab6 - Leonardo Jose Ambrosio - 200657215 Date: Feb/14/2026 Comment: Copying instructions.

---
+++ Lab6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program{
+
+    public static void Main(string[] args) {
+
+        // STEP 1: Declare a new array (of type string), and initialize it with anywhere from 7 to 9 different ice cream flavors;
+        // STEP 2: Create a variable to contain the Length property of the array you created in STEP 1
+        // STEP 3: Output a simple sentence to the Console indicating to the user that you are about to list all the ice cream flavors available at your stand using a FOR() loop
+        // STEP 4: Build a FOR() loop to iterate through the array you created in STEP 1, using the variable you created in STEP 2 to control how many times the loop repeats
+        // STEP 5: Inside the FOR() loop, output each element of the array to the Console
+        // STEP 6: Output a simple sentence to the Console indicating to the user that you are about to list all the ice cream flavors available at your stand using a FOREACH() loop 
+        // STEP 7: Construct a FOREACH() loop that also outputs all of the elements of the same array
+        // STEP 8: Inside the FOREACH() loop, output each element of the array to the Console
+
+
+    }
+
+
+
+
+}
