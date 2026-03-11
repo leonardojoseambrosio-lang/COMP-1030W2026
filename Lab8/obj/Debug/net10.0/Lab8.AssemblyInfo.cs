43066362409eedc01aebed9859f7f35582d1ea25
leonardojoseambrosio-lang <leonardojoseambrosio@gmail.com>
2026-03-10 22:01:22 -0400

    Name: Leonardo Jose Ambrosio ID: 200657215 Date: Mar/10/2026 Description: Testing - date: false;

--- Lab8/obj/Debug/net10.0/Lab8.AssemblyInfo.cs
+++ Lab8/obj/Debug/net10.0/Lab8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eb43f702d7d6f89d17a3a90237f327318555fb1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6fa52b16507c337337c8c73036f05c9d80d2ae05")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
