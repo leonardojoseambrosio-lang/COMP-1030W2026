944c60df08732e9c43bad8affe164616c26a551a
leonardojoseambrosio-lang <leonardojoseambrosio@gmail.com>
2026-03-12 19:07:30 -0400

    Name: Leonardo Jose Ambrosio ID: 200657215 Date: Mar/12/2026 Description: Testing Program.

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -10,14 +10,30 @@ class Program {
         VSCode for this.
         Requirements
        */
-
+      
         //- Generate a random temperature between 10 and 40.
         Random randomNum = new Random();
         int rnd = randomNum.Next(10, 41);//creates a random number between 10 and 40.
         
         Console.WriteLine(rnd);
 
-//- Ask the user to guess the temperature.
+        //- Ask the user to guess the temperature.
+        Console.WriteLine("### Welcome to the Temperature History Game ###");
+        string typeYourAnsewer = Console.ReadLine();
+
+        int[] guess = new int[10];
+
+        if (Int32.TryParse(typeYourAnsewer, out guess[0]))
+        {
+
+            guess[0] = Int32.Parse(typeYourAnsewer);
+        }
+        else { 
+            Console.WriteLine("Type a number.");
+        }
+  
+        Console.WriteLine(guess[0]);
+
 //- Provide feedback:
 //- Too warm
 //- Too cold
