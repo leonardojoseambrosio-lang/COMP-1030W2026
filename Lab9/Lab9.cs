30d107462d88de3a06df66c9868ae2c2af3009c3
leonardojoseambrosio-lang <leonardojoseambrosio@gmail.com>
2026-03-16 16:26:33 -0400

    Name: Leonardo Jose Ambrosio ID: 200657215 Date: Mar/10/2026 Lab 9 - COMP 1030 Programming Fundamentals Description: Constructor created.

--- Lab9/Lab9.cs
+++ Lab9/Lab9.cs
@@ -6,12 +6,20 @@ namespace Lab9
 // STEP 5: Create a class called 'Ball'
 public class Ball
 {
-
+   
     // STEP 6: The ball object needs 3 properties: Color, Size, and Material - declare these below
+    public string Color { get;}
+    public int Size { get; }
+    public string Material { get; }
     // STEP 7a: The CONSTRUCTOR for the ball object accepts 3 arguments (string color, int size, string material) that are used to set the above properties - build the constructor for Ball
-    {
-        // STEP 7b: Use each of the arguments passed in to the above constructor parameters to set the three properties 
-    }
+   public Ball(string color, int size, string material)
+        {
+            // STEP 7b: Use each of the arguments passed in to the above constructor parameters to set the three properties
+            this.Color = color;
+            this.Size = size;
+            this.Material = material;
+
+        }
     // STEP 8a: Create a method called 'Description' - it does not need any parameters and it will not return anything
     {
         // STEP 8b: Build a sentence that describes the Ball object, including all three properties
