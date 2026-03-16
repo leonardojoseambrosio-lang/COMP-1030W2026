29500d4fb7973457e811dc824d6627fdf6d6933b
leonardojoseambrosio-lang <leonardojoseambrosio@gmail.com>
2026-03-16 16:09:13 -0400

    Name: Leonardo Jose Ambrosio ID: 200657215 Date: Mar/10/2026 Lab 9 - COMP 1030 Programming Fundamentals Description: Instructions copied.

--- Assignment2/obj/Debug/net10.0/Assignment2.AssemblyInfo.cs
+++ Assignment2/obj/Debug/net10.0/Assignment2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+797ee178c8b6737ece429abe10028041ad925dd5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b2b9f999a60fb05ec8f3184a5e023da28044c4a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lab9/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Lab 9
+This lab will give you more practice building a class, then creating an instance of that class (an object)
+Your job is to create a Class for a ball object. The class will feature the following properties:
+1. Color
+2. Size (diameter)
+3. Material
+Then you will create a method for the class that will output a simple description of the ball object to the console
+*/
+// STEP 1: Add a dependency with the 'using' directive - we will call the namespace 'Lab9'
+// STEP 2: We will add a new class in a separate file - go to Project > Add Class... in Visual Studio and create a new class called Lab9.cs
+// STEP 3: Copy the code for Lab9.cs (just below) and paste it into the new file - proceed to that file for STEP 4
+// STEP 9: Create an instance of class Ball and call it 'ball1' - it will be red, 10 cm in diameter, and rubber
+// STEP 10: Call the Description method using the ball1 object instance
+// STEP 11: Create another instance of a Ball object, calling it 'ball2' - you will choose the 3 arguments
+// STEP 12: Call the Description method using the ball2 object instance
+// STEP 13: Paste the code for both Program.cs and Lab9.cs into the submission text area below. Please use the "Code snippet" (</>) button so that your code formatting is retained.
